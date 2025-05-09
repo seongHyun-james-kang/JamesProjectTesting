5da943c09a15fdae25d47d1139a4d67e5fd2ce06
James Kang <167275921+seongHyun-james-kang@users.noreply.github.com>
2025-05-09 16:58:20 -0400

    csrf notes added

--- frontend/src/store/csrf.js
+++ frontend/src/store/csrf.js
@@ -1,6 +1,13 @@
 // frontend/src/store/csrf.js
+// This is a very important piece to connect your frontend to a CSRF-protected backend
+// This file creates a special version of fetch, called csrfFetch
+// This automatically adds a CSRF token to your requests if needed
+// Remember that your backend is protected with CSRF tokens. instead of manually
+// adding this token every time, use fetch(), which will do it for you automatically
 
-import Cookies from 'js-cookie';
+
+
+import Cookies from 'js-cookie'; // your backend will send a cookie called XSRF-TOKEN
 
 export async function csrfFetch(url, options = {}) {
   // set options.method to 'GET' if there is no method
@@ -11,10 +18,10 @@ export async function csrfFetch(url, options = {}) {
   // if the options.method is not 'GET', then set the "Content-Type" header to
   // "application/json", and set the "XSRF-TOKEN" header to the value of the
   // "XSRF-TOKEN" cookie
-  if (options.method.toUpperCase() !== 'GET') {
-    options.headers['Content-Type'] =
-      options.headers['Content-Type'] || 'application/json';
-    options.headers['XSRF-Token'] = Cookies.get('XSRF-TOKEN');
+  if (options.method.toUpperCase() !== 'GET') { // this will only run for non-GET request(POST,PUT,DELETE, ETC)
+    options.headers['Content-Type'] = // if content-type exists leave it alone. other wise set it to 'application/json'
+      options.headers['Content-Type'] || 'application/json'; //  because most PUT/POST requests send JSON data. without 'content-type':'application/json', the backend wouldn't know the body is JSON
+    options.headers['XSRF-Token'] = Cookies.get('XSRF-TOKEN'); // puts into a new header called 'XSRF-Token'
   }
   // call the default window's fetch with the url and the options passed in
   const res = await window.fetch(url, options);
