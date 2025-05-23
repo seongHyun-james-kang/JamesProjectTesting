57979b0f3ac7b891366a43c6421b11635d9a3000
James Kang <167275921+seongHyun-james-kang@users.noreply.github.com>
2025-05-22 23:30:29 -0400

    added comments for index.js

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,9 +1,18 @@
 //frontend/src/store/index.js
+// Import necessary functions from Redux to create the store and middleware
+
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+// Redux Thunk lets us write functions that return functions (to handle async work like fetching from APIs)
+
 import thunk from 'redux-thunk';
+// Import the session slice of state
+
 import sessionReducer from './session';
+// Import the spots slice of state 
+
 import spotsReducer from './spots';
 
+// Combine all reducers into one big reducer (aka the root reducer)
 const rootReducer = combineReducers({
   session: sessionReducer,
   spots: spotsReducer
@@ -11,6 +20,7 @@ const rootReducer = combineReducers({
 
 let enhancer;
 
+// In production (live site), only use thunk middleware (no logging or dev tools)
 if (import.meta.env.MODE === 'production') {
   enhancer = applyMiddleware(thunk);
 } else {
@@ -20,8 +30,14 @@ if (import.meta.env.MODE === 'production') {
   enhancer = composeEnhancers(applyMiddleware(thunk, logger));
 }
 
+
+
+
+// This function sets up and returns the Redux store
 const configureStore = (preloadedState) => {
   return createStore(rootReducer, preloadedState, enhancer);
 };
 
 export default configureStore;
+
+
