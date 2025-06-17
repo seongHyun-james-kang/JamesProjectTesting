9d4d06b6caaee839d6e5c8188cc1d6170617fd35
James Kang <167275921+seongHyun-james-kang@users.noreply.github.com>
2025-06-16 23:28:29 -0400

    fixed images

--- backend/db/seeders/20250321005826-demo-review-images.js
+++ backend/db/seeders/20250321005826-demo-review-images.js
@@ -15,19 +15,19 @@ module.exports = {
     await ReviewImage.bulkCreate([
       {
         reviewId: 1,  // Make sure this ID exists in the Reviews table
-        url: "https://example.com/review-image1.jpg",
+        url: "https://www.constructionspecifier.com/wp-content/uploads/2023/07/ankrom-moisan-mass-timber-building-ai-rendering-1.jpg",
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
         reviewId: 2,  // Make sure this ID exists in the Reviews table
-        url: "https://example.com/review-image2.jpg",
+        url: "https://cwimages.imgix.net/BuildingImages/Ashworth2023/AW-Ext-03.jpg?auto=compress",
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
         reviewId: 3,
-        url: "https://example.com/review-image3.jpg",
+        url: "https://images.unsplash.com/photo-1508739773434-c26b3d09e071",
         createdAt: new Date(),
         updatedAt: new Date(),
       },
